ed0fe2211f561f26546ef54f7d503280711f63ca
zab3 <xelvis03@gmail.com>
2020-09-27 18:19:09 +0900

    Update memo.js

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,7 @@
 function memo(){
   const submit = document.getElementById("submit");
   submit.addEventListener("click",(e) => {
-    const formData = new FromData(document.getElementById("form"));
+    const formData = new FormData(document.getElementById("form"));
     const XHR = new XHLHttpRequest();
     XHR.open("POST","/posts",true);
     XHR.responseType = "json";
